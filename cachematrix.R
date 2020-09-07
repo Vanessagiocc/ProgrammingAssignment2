2796edf48ceaeb47fd2c7f3f690d265ff20ed4bc
Vanessa Giovanna Contreras Cerda <vanessagiocc@gmail.com>
2020-09-07 19:35:16 -0300

    Tarea completada

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-hola
+inv<-NULL
+set<-function(y){
+  x<<-y
+  inv<-NULL
+}
+get<-function() x
+setinverse<-function(inverse)inv<-inverse 
+getinverse<-function() inv
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
@@ -12,4 +20,14 @@ hola
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinverse()
+  if(!is.null(inv)){
+    message("getting cache data")
+    return(inv)
+  }
+  else
+    data<-x$get()
+  inv<-solve(data,...)
+  x$setinverse(inv)
+  inv
 }
